95db5c87e7f4ac4db521616848703d604a2adcff
alisha-web <au630097@gmail.com>
2024-08-20 22:33:02 +0500

    Add files via upload

---
+++ .js
@@ -0,0 +1,166 @@
+// alert("pakistan zindabad");
+
+// console.log("pakistan zindabad");
+
+// console.log("pakistan zindabad");
+
+//  var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver)
+
+// var whatsLeftOver = 9 % 3;
+// alert(whatsLeftOver)
+
+// var fulname= prompt("enter your name");
+// console.log(fulname);
+
+// var num1= +prompt("enter first number");
+// var num2= +prompt("enter second number");
+// var num3= +prompt("enter third number");
+// var addition= num1+num2+num3;
+// console.log(addition);
+
+
+// var city= prompt("enter where do you live: ");
+// var score=0;
+
+// if (city=="karachi"){
+//     alert("you are in my city"+ ++score);
+// }
+// else{
+//     alert("you are out of my city!"+ --score);
+// }
+
+// var age= prompt("enter your age");
+// if(age>18){
+//     console.log("you are at legal age");
+// }
+// else{
+//     console.log("you are at immature");
+// }
+
+// var age= prompt("enter your age");
+// if(age>=18){
+//     console.log("you are at legal age");
+// }
+// else{
+//     console.log("you are at immature");
+// }
+
+// var rollno= prompt("enter your rollno");
+// if(rollno==1084){
+//     console.log("you can give exam");
+// }
+// else{
+//     console.log("you cant give exam");
+// }
+
+// var rollno= +prompt("enter your rollno");
+// if(rollno===1084){
+//     console.log("you can give exam");
+// }
+// else{
+//     console.log("you cant give exam");
+// }
+
+// *********************== double equal means both sides eaual and === triple equal means that both side shouls
+//               equal plus datatype should equal ****************************
+
+// var rollno= +prompt("enter your rollno");
+// if(rollno !== 1084){
+//     console.log("fees clear, you can give exam");
+// }
+// else{
+//     console.log("pay your fees than appear in exam");
+// }
+
+// var userage= +prompt ("enter your age");
+// var usergender= prompt ("enter your gender");
+// var userweight= +prompt ("enter your weight");
+
+// if(userage>=20 && usergender=="male" || userweight<=300){
+//     alert("come to our gym")
+// }
+// else{
+//     alert("next time please!")
+// }
+
+// var firstnum = +prompt("enter your first number");
+// var secondnum = +prompt("enter your second number");
+// var sign = prompt("enter your sign");
+
+// if(sign==="+"){
+//     console.log(firstnum + secondnum);
+// }
+// else if(sign==="-"){
+//     console.log (firstnum - secondnum);
+// }
+// else if(sign==="*"){
+//     console.log (firstnum * secondnum);
+// }
+// else if(sign==="/"){
+//     console.log (firstnum / secondnum);
+// }
+// else {
+//     console.log ("error");
+// }
+
+
+// var x=1;
+// var y=66;
+// var a=3;
+// var b=4;
+// var c=6;
+// var d=6;
+// var e=7;
+// var f=8;
+// var g=9;
+// var h=10;
+// if(c===d){
+//     if(x===y){
+//         g=h;
+//         console.log(g);
+//         console.log(h);
+//     }
+//     else if(a===b){
+//         g=h;
+//         console.log(x);
+//         console.log(y); 
+//     }
+//     else{
+//         x=y;
+//         console.log(x);
+//         console.log(y);
+//     }
+// }
+// else{
+//     console.log("code is code");
+// }
+
+// Arrays
+// var pets = ["lion","cat","dog","panda"]
+// types0f(pets)
+// pets.push("monkey","donkey","zebra")
+// pets.pop()
+// pets.shift()
+// pets.unshift("horse","crocodile")
+// pets.splice(2,3,"cheeta","fox","gorila")
+// pets[4]
+// pets.length
+// pets[11]="snake"
+// pets.splice(7,4)
+
+// var user = +prompt("enter your number")
+// if(user % 2 == 0 ){
+//     document.write("this is an even number");
+// }
+// else if(user % 3 == 0){
+//     document.write("this is an odd number");
+// }
+// else{
+//     document.write("prime number");
+// }
+
+
+for (var i = 0 ; i <= 10 ; i ++){
+    document.write( i + "hello world" , "<br>");
+}
