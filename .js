e3ff3b39ef525857065b115ad5d29b2d95233da4
alisha-web <au630097@gmail.com>
2024-08-20 23:26:23 +0500

    Update .js

--- .js
+++ .js
@@ -1,166 +1,227 @@
-// alert("pakistan zindabad");
+// **************************Chapter 21 (Changing Case)**************************
 
-// console.log("pakistan zindabad");
+// 1. Type the characters that are missing from this code.
+// var allLower = userInput.toLowerCase;
+// Note: Correct this statement by yourself.
+var allLower = userInput.toLowerCase();
 
-// console.log("pakistan zindabad");
+// 2. Convert the string represented by x to lower-case and assign the
+// result to the same variable.
+x = x.toLowerCase();
 
-//  var whatsLeftOver = 10 % 3;
-// alert(whatsLeftOver)
+// 3. Convert the string represented by y to upper-case and assign the
+// result to the same variable.
+y = y.toUpperCase();
 
-// var whatsLeftOver = 9 % 3;
-// alert(whatsLeftOver)
+// 4. Convert the string represented by a variable to lower-case and
+// assign the result to a second variable that hasn't been declared
+// beforehand.
+var newVar = someString.toLowerCase();
 
-// var fulname= prompt("enter your name");
-// console.log(fulname);
+// 5. Convert the string represented by an array element to lower-case
+// and assign it to a variable that hasn't been declared beforehand.
+var lowerArrayElement = myArray[0].toLowerCase();
 
-// var num1= +prompt("enter first number");
-// var num2= +prompt("enter second number");
-// var num3= +prompt("enter third number");
-// var addition= num1+num2+num3;
-// console.log(addition);
+// 6. Display in an alert the upper-case version of the string
+// represented by a variable.
+alert(myVar.toUpperCase());
 
+// 7. var cityName = “kaRacHi”;
+// Convert the string represented by a cityName in Capitalisation is
+// the writing of a word with its first letter in uppercase and the
+// remaining letters in lowercase.
+var cityName = "kaRacHi";
+cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
 
-// var city= prompt("enter where do you live: ");
-// var score=0;
+// ******************************Chapter 22 - 25 (Strings)******************************
 
-// if (city=="karachi"){
-//     alert("you are in my city"+ ++score);
-// }
-// else{
-//     alert("you are out of my city!"+ --score);
-// }
+// 1. "captain" has been assigned to variable “sameWords”. You want
+// to slice "ap" out of it.
+var sameWords = "captain";
+var sliced = sameWords.slice(1, 3); // "ap"
 
-// var age= prompt("enter your age");
-// if(age>18){
-//     console.log("you are at legal age");
-// }
-// else{
-//     console.log("you are at immature");
-// }
+// 2. The number of characters in the string will be assigned to the
+// variable.
+var charCount = myString.length;
 
-// var age= prompt("enter your age");
-// if(age>=18){
-//     console.log("you are at legal age");
-// }
-// else{
-//     console.log("you are at immature");
-// }
+// 3. The string "elephant" has been assigned to the variable animal.
+// Slice the four middle characters out of the string and assign it to
+// the variable seg, which hasn't been declared beforehand.
+var animal = "elephant";
+var seg = animal.slice(2, 6); // "epha"
 
-// var rollno= prompt("enter your rollno");
-// if(rollno==1084){
-//     console.log("you can give exam");
-// }
-// else{
-//     console.log("you cant give exam");
-// }
+// 4. Find the number of characters in the string represented by a
+// variable and assign the number to a second variable.
+var charCount = myString.length;
+var anotherVar = charCount;
 
-// var rollno= +prompt("enter your rollno");
-// if(rollno===1084){
-//     console.log("you can give exam");
-// }
-// else{
-//     console.log("you cant give exam");
-// }
+// 5. In a first statement measure how many characters there are in a
+// string represented by a variable. In a second statement slice all
+// but the first character and last 3 characters of the string and
+// assign it to a second variable that hasn't been declared
+// beforehand.
+var charCount = myString.length;
+var newString = myString.slice(1, charCount - 3);
 
-// *********************== double equal means both sides eaual and === triple equal means that both side shouls
-//               equal plus datatype should equal ****************************
+// 6. var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// What is the value of indx?
+var text = "To be or not to be.";
+var indx = text.indexOf("be"); // 3
 
-// var rollno= +prompt("enter your rollno");
-// if(rollno !== 1084){
-//     console.log("fees clear, you can give exam");
-// }
-// else{
-//     console.log("pay your fees than appear in exam");
-// }
+// 7. var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// What is the value of indx?
+// Note: Try the above both examples by yourself.
+var text = "To be or not to be.";
+var indx = text.lastIndexOf("be"); // 16
 
-// var userage= +prompt ("enter your age");
-// var usergender= prompt ("enter your gender");
-// var userweight= +prompt ("enter your weight");
+// 8. Find the index of the first character of the last instance of "go" in
+// the string represented by the variable text and assign the number
+// to the variable indx, which hasn't been declared beforehand.
+var indx = text.lastIndexOf("go");
 
-// if(userage>=20 && usergender=="male" || userweight<=300){
-//     alert("come to our gym")
-// }
-// else{
-//     alert("next time please!")
-// }
+// 9. Code the first line of an if statement that tests whether a segment
+// with an index represented by indexNum exists in a string.
+if (myString.indexOf(segment, indexNum) !== -1) {
 
-// var firstnum = +prompt("enter your first number");
-// var secondnum = +prompt("enter your second number");
-// var sign = prompt("enter your sign");
+// 10. In this string "abcde", what character is at index 2? (Use
+// charAt)
+var char = "abcde".charAt(2); // "c"
 
-// if(sign==="+"){
-//     console.log(firstnum + secondnum);
-// }
-// else if(sign==="-"){
-//     console.log (firstnum - secondnum);
-// }
-// else if(sign==="*"){
-//     console.log (firstnum * secondnum);
-// }
-// else if(sign==="/"){
-//     console.log (firstnum / secondnum);
-// }
-// else {
-//     console.log ("error");
-// }
+// 11. Find the 10th character in the string represented by text and
+// assign it to the variable cha, which hasn't been declared
+// beforehand.
+var cha = text.charAt(9);
 
+// 12. Find the last character in the string represented by str and
+// assign it to x, which hasn't been declared beforehand.
+var x = str.charAt(str.length - 1);
 
-// var x=1;
-// var y=66;
-// var a=3;
-// var b=4;
-// var c=6;
-// var d=6;
-// var e=7;
-// var f=8;
-// var g=9;
-// var h=10;
-// if(c===d){
-//     if(x===y){
-//         g=h;
-//         console.log(g);
-//         console.log(h);
-//     }
-//     else if(a===b){
-//         g=h;
-//         console.log(x);
-//         console.log(y); 
-//     }
-//     else{
-//         x=y;
-//         console.log(x);
-//         console.log(y);
-//     }
-// }
-// else{
-//     console.log("code is code");
-// }
+// 13. Find the the 5th character in a string represented by input
+// and assign it to cha, which hasn't been declared beforehand.
+var cha = input.charAt(4);
 
-// Arrays
-// var pets = ["lion","cat","dog","panda"]
-// types0f(pets)
-// pets.push("monkey","donkey","zebra")
-// pets.pop()
-// pets.shift()
-// pets.unshift("horse","crocodile")
-// pets.splice(2,3,"cheeta","fox","gorila")
-// pets[4]
-// pets.length
-// pets[11]="snake"
-// pets.splice(7,4)
+// 14. Code the first line of an if statement that tests whether the
+// 3rd character of a string represented by a variable is a particular
+// character.
+if (myString.charAt(2) === 'specificChar') {
 
-// var user = +prompt("enter your number")
-// if(user % 2 == 0 ){
-//     document.write("this is an even number");
-// }
-// else if(user % 3 == 0){
-//     document.write("this is an odd number");
-// }
-// else{
-//     document.write("prime number");
-// }
-
-
-for (var i = 0 ; i <= 10 ; i ++){
-    document.write( i + "hello world" , "<br>");
+// 15. Code a for loop that cycles through all the characters of a
+// string represented by a variable and assigns each character to an
+// element of an array that has been declared beforehand.
+// In the string represented by reply replace the first instance of "no"
+// with "yes" and assign the revised string to revisedReply, which hasn't
+// been declared beforehand.
+var arr = [];
+for (var i = 0; i < myString.length; i++) {
+    arr[i] = myString.charAt(i);
 }
+
+// 16. In a string represented by str replace the first instance of "1"
+// with "one" and assign the revised string to newStr, which hasn't
+// been declared beforehand.
+var revisedReply = reply.replace("no", "yes");
+
+// 17. If you want all instances replaced, enter 3 characters that
+// need to appear in this statement.
+// var y = x.replace("a", "z");
+var newStr = str.replace("1", "one");
+
+// ******************************Chapter 26 (Rounding Numbers)******************************
+
+// 1. Form a statement that rounds a number to the nearest integer.
+var roundedNum = Math.round(num);
+
+// 2. Round up a number represented by origNum and assign it to
+// roundNum, which hasn't been declared beforehand.
+var roundNum = Math.ceil(origNum);
+
+// 3. Round down a number represented by origNum and assign it to
+// roundNum, which hasn't been declared beforehand.
+var roundNum = Math.floor(origNum);
+
+// 4. Round a number represented by a variable and assign the result
+// to a second variable that hasn't been declared beforehand.
+var roundedVar = Math.round(origNum);
+
+// 5. Round .5 to 0 and assign it to a variable that hasn't been declared
+// beforehand.
+var rounded = Math.floor(0.5);
+
+// ******************************Chapter 27 (Random Numbers)******************************
+
+// 1. Convert a random number generated by JavaScript to a number in
+// the range 1 to 50
+var randomNum = Math.floor(Math.random() * 50) + 1;
+
+// 2. Generate a random number and assign it to a variable that hasn't
+// been declared beforehand.
+var randomNum = Math.random();
+
+// 3. You have to create a dice in JavaScript with the use of pseudo-
+// random number.
+var diceRoll = Math.floor(Math.random() * 6) + 1;
+
+// 4. You have to create a toss (head/tail) in JavaScript with the use of
+// pseudo-random number.
+var toss = Math.random() < 0.5 ? 'heads' : 'tails';
+
+// ******************************Chapter 28, 29 (Converting Strings)******************************
+
+// 1. How do you convert a string to an integer in JavaScript?
+var intNum = parseInt("123");
+
+// 2. Write a JavaScript function to convert the string "123" to an
+// integer.
+function convertToInt(str) {
+    return parseInt(str);
+}
+
+// 3. How can you convert a string containing a decimal number to a
+// floating-point number in JavaScript?
+var floatNum = parseFloat("123.45");
+
+// 4. How can you check if a string can be successfully converted to an
+// integer or decimal in JavaScript before performing the
+// conversion?
+function canConvertToNumber(str) {
+    return !isNaN(parseFloat(str)) && isFinite(str);
+}
+
+// 5. How can you convert a number to a string in JavaScript?
+var strNum = num.toString();
+
+// 6. Write a JavaScript function to convert the number 42 to a string.
+function convertToString(num) {
+    return num.toString();
+}
+
+// 7. Can you convert a string representing a decimal number (e.g.,
+//     "3.14") to an integer in JavaScript? If so, how?
+var intNum = parseInt("3.14");
+
+// *********Chapter 30 (Controlling the length of decimals)******************************
+
+// 1. Code a statement that rounds a number represented by num to 4
+// places, converts it to a string, and assigns it to newNum, which
+// hasn't been declared beforehand.
+var newNum = num.toFixed(4).toString();
+
+// 2. In a single statement round a number represented by a variable to
+// 2 places, convert it to a string, convert it back to a number, and
+// assign it to the same variable.
+num = parseFloat(num.toFixed(2));
+
+// 3. Code the first line of an if statement that tests whether the
+// number represented by num, rounded to 2 digits and converted
+// to a string, has more than 4 characters in it.
+if (num.toFixed(2).toString().length > 4) {
+
+// 4. Assign a number with many decimal places to a variable.
+// Code an alert that displays the number rounded to 2 decimal
+// places and converted to a string.
+var num = 3.14159265359;
+alert(num.toFixed(2).toString());
+
+// ****************************** THE END ******************************
